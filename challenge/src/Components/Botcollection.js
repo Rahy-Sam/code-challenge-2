574f74bad71a08ec883798bceaade3a81c7a5e2c
Rahy-Sam <145539129+Rahy-Sam@users.noreply.github.com>
2023-12-01 11:52:30 +0300

    Update Botcollection.js

--- challenge/src/Components/Botcollection.js
+++ challenge/src/Components/Botcollection.js
@@ -1,28 +1,60 @@
+import React, { useEffect, useState } from "react";
+import BotMap from "./BotMap";
+import BotSpecs from "./BotSpecs";
+import SortBar from "./SortBar";
+import BotArmy from "./BotArmy";
 
-import React from "react";
+const BotCollection = () => {
+  const [data, setData] = useState([]); //create the state of the current data
+  const [selectedCard, setselectedCard] = useState(null); // create state of the selected card
 
-function Botcollection({ bots, onBotClick }) {
-    // Render the Botcollection component
-    return (
-        <>
-            <h1>Bot collection </h1>
-            {/* Container for the collection of bots */}
-            <div id="botCollection">
-                {/* Map through the array of all bots */}
-                {bots.map((bot) => (
-                    // Render each bot as a card with a click event handler
-                    <div key={bot.id} className="bot-card" onClick={() => onBotClick(bot)}>
-                        <img src={bot.avatar_url} alt={bot.name} />
-                        <p>{bot.name}</p>
-                        <p>Class: {bot.bot_class}</p>
-                        <p>Health: {bot.health}</p>
-                        <p>Damage: {bot.damage}</p>
-                        <p className="catchphrase">{bot.catchphrase}</p>
-                    </div>
-                ))}
-            </div>
-        </>
-    );
-}
+  const handleClick = (card) => {
+    setselectedCard(card); //  function that handles the display of the selectedcard
+  };
 
-export default Botcollection;
+  const handleClose = () => {
+    setselectedCard(null); //  function to close the selected card
+  };
+
+  const [enlistbot, setEnlistedbot] = useState([]); // state to manage the  enlisted bot
+
+   const handleenlistbutton = (card) => {
+    const BotEnlisted = enlistbot.every((enlist) => enlist.card_class === card.card_class);
+    if(BotEnlisted){
+      setEnlistedbot((previousenlistbot) => [...previousenlistbot, card])
+    }
+     
+   };
+   const buttoncloseenlist = (bots) => {
+    setEnlistedbot((previousenlistbot) => previousenlistbot.filter((bot)=> bot.id !== bots.id));
+   }
+
+  //Fetch data
+  const fetchData = () => {
+    fetch("https://bot-x0k6.onrender.com/bots")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => console.log("Error fetching", error));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+
+      <BotArmy onCardClose={buttoncloseenlist} card={enlistbot}/>
+      <SortBar data={data} setData={setData} />
+      {/* if card is selected render on the BotSpecs */}
+      {selectedCard ? (
+        <BotSpecs card={selectedCard} onClose={handleClose} onClick={handleenlistbutton} />
+      ) : (
+        <BotMap data={data} onCardClick={handleClick} />
+      )}
+     
+    </div>
+  );
+};
+
+export default BotCollection;
